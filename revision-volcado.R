eb3c513e4c7eab8234647c794574bb5926521a1d
Eucrow <moko@sierpe.org>
2016-06-28 14:25:57 +0200

    Add version number

--- revision-volcado.R
+++ revision-volcado.R
@@ -5,8 +5,8 @@
 ####
 #### author: Marco A. Amez Fernandez
 #### email: ieo.marco.a.amez@gmail.com
-#### date of last modification: 24/6/2016
-#### version: 1.3
+#### date of last modification: 28/6/2016
+#### version: 1.4
 ####
 #### files required: esp mezcla.csv, especies_no_mezcla.csv,
 #### estratorim_arte.csv, divisiones.csv, especies_no_permitidas.csv
