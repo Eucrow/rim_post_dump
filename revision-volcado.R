375776168bdebd6394beff257431919dc31571d7
Eucrow <moko@sierpe.org>
2017-02-20 14:42:18 +0100

    Add variable to export in coherenceEstratoRimGear()

--- revision-volcado.R
+++ revision-volcado.R
@@ -175,9 +175,9 @@ formatErrorsList <- function(errors_list = ERRORS){
 
 # Function to check the coherence between 'ESTRATO_RIM' and 'gear' -------------
 coherenceEstratoRimGear <- function(df){
-  merge_estrato_rim_gear<-merge(x=df, y=CORRECT_ESTRATORIM_ARTE, by.x = c("ESTRATO_RIM","ARTE"), by.y = c("ESTRATO_RIM", "ARTE"), all.x = TRUE)
+  merge_estrato_rim_gear<-merge(x=df, y=CORRECT_ESTRATORIM_ARTE, by.x = c("ESTRATO_RIM", "ARTE"), by.y = c("ESTRATO_RIM", "ARTE"), all.x = TRUE)
   incoherent_data <- -which(merge_estrato_rim_gear[["VALID"]])
-  incoherent_data <- merge_estrato_rim_gear[incoherent_data,c(BASE_FIELDS, "ARTE")]
+  incoherent_data <- merge_estrato_rim_gear[incoherent_data,c(BASE_FIELDS, "COD_ARTE.x", "ARTE")]
   incoherent_data <- unique(incoherent_data)
   incoherent_data <- addTypeOfError(incoherent_data, "ERROR: no concuerda el estrato_rim con el arte")
   return(incoherent_data)
