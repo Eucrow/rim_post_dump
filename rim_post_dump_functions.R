9cca179a0bcaaec9274ade1c2c0d7f33faf12038
Alberto Candelario <alberto.candelario@st.ieo.es>
2024-02-16 14:14:31 +0100

    adding checkShipNoMasterFishingGear and checkShipDifferentFishingGear functions

--- rim_post_dump_functions.R
+++ rim_post_dump_functions.R
@@ -1723,6 +1723,127 @@ checkPrioritySpeciesSampled <- function(catches, lengths){
 
 }
 
+#' Check code: 1078
+#' Function to check the presence of the ships from our ships' masterdata on the 
+#' working data
+#' @param catches: data frame returned by the importRIMCatches() or
+#' importRIMFiles() functions.
+#' @param shipFishingGearMaster: master dataframe with the code ships and its 
+#' ESTRATO_RIM from 2020 to 2022.
+#' @return Data frame with those ships no presents in our working data, labeled 
+#' with a WARNING message
+
+checkShipNoMasterFishingGear <- function(catches, shipFishingGearMaster){
+  
+  #Step 1: filter the necessary columns from the catches df and pass from level to normal data
+  
+  catches <- catches[, BASE_FIELDS]
+  
+  catches$COD_BARCO <- as.character(catches$COD_BARCO)
+  
+  catches$ESTRATO_RIM <- as.character(catches$ESTRATO_RIM)
+  
+  #Step 2: filter the ships in the catches' dataframe that are not present in boatFishingArtMaster
+  
+  masterShip <- unique(shipFishingGearMaster$COD_BARCO)
+  
+  catchesNoMasterShip <- catches[!(catches$COD_BARCO %in% masterShip), ]
+  
+  #Step 3: check that the number of road is not equal to zero and add the type or error for obtained dataframes
+  
+  if (nrow(catchesNoMasterShip)!=0){
+    
+    catchesNoMasterShip <- addTypeOfError(catchesNoMasterShip, "WARNING: Barco no presente en el maestro")
+    
+  }
+  
+  catchesNoMasterShip <- unique(catchesNoMasterShip)
+  
+  return(catchesNoMasterShip)
+  
+}
+
+#' Check code: 1079
+#' Function to check the coherence of the ESTRATO_RIM between our ships' masterdata  
+#' and the working data
+#' @param catches: data frame returned by the importRIMCatches() or
+#' importRIMFiles() functions.
+#' @param shipFishingGearMaster: master dataframe with the code ships and its 
+#' ESTRATO_RIM from 2020 to 2022.
+#' @return Data frame that mark those ships where there is not coincidence between the
+#' ESTRATO_RIM from our ships' masterdata and the working data, specifying those what does not
+#' match with a Warning message
+
+checkShipDifferentFishingGear <- function(catches, shipFishingGearMaster){
+  
+  #Step 1: add the "Testigo" column to shipFishingGearMaster and convert "COD_BARCO" and "ESTRATO_RIM" as parameter
+  
+  shipFishingGearMaster$TESTIGO <- "T"
+  
+  shipFishingGearMaster$COD_BARCO <- as.character(shipFishingGearMaster$COD_BARCO)
+  
+  shipFishingGearMaster$ESTRATO_RIM <- as.character(shipFishingGearMaster$ESTRATO_RIM)
+  
+  #Step 2: filter the necessary columns from the catches df and pass from level to normal data
+  
+  catches <- catches[, BASE_FIELDS]
+  
+  catches$COD_BARCO <- as.character(catches$COD_BARCO)
+  
+  catches$ESTRATO_RIM <- as.character(catches$ESTRATO_RIM)
+  
+  #Step 3: filter the ships in the catches' dataframe that are present in shipFishingGearMaster
+  
+  masterShip <- unique(shipFishingGearMaster$COD_BARCO)
+  
+  catchesMasterShip <- catches[catches$COD_BARCO %in% masterShip, ]
+  
+  #Step 4: merging the shipFishingGearMaster frame with the catches' one
+  
+  catchesMasterShip <- merge(catchesMasterShip, shipFishingGearMaster, all.x=TRUE)
+  
+  #Step 5: develop new dataframes, one to detect where the TESTIGO values are "Na"
+  
+  catchesNaValues <- unique(catchesMasterShip[is.na(catchesMasterShip$TESTIGO), ])
+  
+  #Step 6: now, we make a subset of the masterShip with the ship's codes of the catchesNaValues.  
+  #Then, we create a special dataframe where we have unite all ESTRATOS that has one ship
+  #using the function «concatenaThor». 
+  
+  shipFishingGearMasterNaValues <- shipFishingGearMaster[shipFishingGearMaster$COD_BARCO %in% catchesNaValues$COD_BARCO, 
+                                                         c("COD_BARCO","ESTRATO_RIM")]
+  
+  fusionEstratos <- tapply(shipFishingGearMasterNaValues$ESTRATO_RIM, 
+                           shipFishingGearMasterNaValues$COD_BARCO, 
+                           paste, collapse=", ")
+  
+  shipCodes <- unique(shipFishingGearMasterNaValues$COD_BARCO)
+  
+  fusionEstratosDataFrame <- data.frame(COD_BARCO=shipCodes, ESTRATOS=fusionEstratos)
+  
+  #Step 7: here we merge the fusionEstratosDataFrame with the catchesNaValues 
+  
+  catchesNaValues <- merge(catchesNaValues, fusionEstratosDataFrame, all.x=TRUE)
+  
+  #Step 8: check that the number of road is not equal to zero and add the type or error for obtained dataframes
+  
+  if (nrow(catchesNaValues)!=0){
+    message <- paste0("WARNING: El barco trabajó en 2020-2022 en otros estratos diferentes: ", catchesNaValues$ESTRATOS)
+    catchesNaValues$TIPO_ERROR <- message
+  }
+  
+  #Note: making a little fix in the columns of the catchesNaValues
+  
+  catchesNaValues <- catchesNaValues[, c(BASE_FIELDS, "TIPO_ERROR")]
+  
+  catchesNaValues <- unique(catchesNaValues)
+  
+  return(catchesNaValues)
+  
+}
+
+
+
 
 #' Copy all the error files generated to a shared folder. ----
 #' Used to copy errors files generated to the shared folder
