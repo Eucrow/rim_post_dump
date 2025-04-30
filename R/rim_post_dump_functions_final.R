cfa436ebd8465b4c1d8d9b2c2d3a4b8fa88a01fd
MARCO ANTONIO.AMEZ.FERNANDEZ.922048 <marco.amez@ieo.csic.es>
2025-04-30 13:10:52 +0200

    Clean code and change names of functions checkCmMeasures and checkHalfCmMeasures.

---
+++ R/rim_post_dump_functions_final.R
@@ -0,0 +1,3 @@
+source(file.path(getwd(), "R", "prepareLengthsForCheckMeasures.R"))
+source(file.path(getwd(), "R", "checkCmMeasures_1075.R"))
+source(file.path(getwd(), "R", "checkHalfCmMeasures_1076.R"))
