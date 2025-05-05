48ade5493526830e914ed274deea1ea907c8b62f
Alberto C. B. <albert.candelario.oficial@gmail.com>
2025-05-05 11:12:09 +0000

    sampling_is_checked function

--- rim_check_annual_nvdp_matched.R
+++ rim_check_annual_nvdp_matched.R
@@ -185,6 +185,8 @@ rim_check_annual_nvdp_matched <- function(samples_imported) {
     #
     # err$check_half_cm_measures <- checkHalfCmMeasures(lengths_sampled)
     #
+    # err$sampling_is_checked <- sampling_is_checked(lengths_sampled)
+    #
     # # comment in annual:
     # # err$with_historical_size_range <- checkRangeInHistorical(muestreos_up$lengths)
     #
--- rim_post_dump_functions.R
+++ rim_post_dump_functions.R
@@ -2143,3 +2143,32 @@ categories99NotInMt2b <- function(catches) {
     return(catches)
   }
 }
+
+#' Check code: 1083
+#' Detect samples that are not validated (column "VALIDADO" from length dataframe
+#' with FALSE or NA values).
+#' @param lengths: catches data frame returned by the importRIMCatchesInLengths() 
+#' or importRIMFiles() functions.
+#' @return Data frame whit errors.
+
+sampling_is_checked <- function(lengths){
+  
+  # Base work columns
+  
+  BASE_FIELDS <- c(BASE_FIELDS, "VALIDADO")
+  
+  # Filter FALSE and/or NA values from "VALIDADO" column
+  
+  lengths <- lengths[is.na(lengths$VALIDADO) | 
+                       lengths$VALIDADO == FALSE, 
+                     BASE_FIELDS]
+  
+  if(nrow(lengths)>0){
+    lengths <- addTypeOfError(
+      lengths,
+      "WARNING: sample is not checked"
+    )
+    return(lengths)
+  }
+}
+
